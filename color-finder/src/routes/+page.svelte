5b6dcce6ebbf70381c08c5a4609d82ccb6fabf48
RishabhSingh08 <Rishabh.home.1@gmail.com>
2023-08-17 20:24:01 -0500

    remove user input on generate color

--- color-finder/src/routes/+page.svelte
+++ color-finder/src/routes/+page.svelte
@@ -5,6 +5,7 @@
   function getColor() {
     bgColor = "#" + Math.random().toString(16).slice(2, 8);
     document.body.style.backgroundColor = bgColor;
+    userColor = "";
   }
 
   function isValidHexColor(color) {
